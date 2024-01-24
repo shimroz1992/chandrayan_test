06b91635ca82bb0235b5770de0c2d8a2c4149e4f
shimroz <shimrozahemed.ngp@gmail.com>
2024-01-24 15:34:29 +0530

    Added provision for move_backward

--- app/models/spacecraft.rb
+++ app/models/spacecraft.rb
@@ -7,4 +7,23 @@ class Spacecraft < ApplicationRecord
     self.x += 1 if direction == 'E'
     self.x -= 1 if direction == 'W'
   end
+
+  def move_backward
+    case direction
+    when 'N' then move_along_axis(:y, -1)
+    when 'S' then move_along_axis(:y, 1, max_bound: 9_999_999)
+    when 'E' then move_along_axis(:x, -1)
+    when 'W' then move_along_axis(:x, 1, max_bound: 9_999_999)
+    end
+  end
+
+  private
+
+  def move_along_axis(axis, step, max_bound: nil)
+    self.send("#{axis}=", self.send(axis) + step) unless max_bound_reached?(axis, step, max_bound)
+  end
+
+  def max_bound_reached?(axis, step, max_bound)
+    max_bound && (self.send(axis) + step).send(axis == :y ? :zero? : :>=, max_bound)
+  end
 end
--- spec/models/spacecraft_spec.rb
+++ spec/models/spacecraft_spec.rb
@@ -20,4 +20,18 @@ RSpec.describe Spacecraft, type: :model do
       expect(spacecraft.y).to eq(1)
     end
   end
+
+  describe '#move_backward' do
+    it 'moves the spacecraft one step backward in the current direction' do
+      spacecraft = Spacecraft.new(x: 0, y: 0, z: 0, direction: 'N')
+      spacecraft.move_backward
+      expect(spacecraft.y).to eq(-1)
+    end
+
+    it 'does not allow the spacecraft to move beyond galactic boundaries' do
+      spacecraft = Spacecraft.new(x: 0, y: 0, z: 0, direction: 'W')
+      spacecraft.move_backward
+      expect(spacecraft.x).to eq(1) # Should not move beyond the boundary
+    end
+  end
 end
