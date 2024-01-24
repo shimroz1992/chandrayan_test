24d0b087a84458343ee330a7000a6d4a25c64b3c
shimroz <shimrozahemed.ngp@gmail.com>
2024-01-24 15:00:11 +0530

    Added provision for move_forward

--- spec/models/spacecraft_spec.rb
+++ spec/models/spacecraft_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Spacecraft, type: :model do
     spacecraft = FactoryBot.build(:spacecraft, x: nil, y: nil, z: nil, direction: nil)
     expect(spacecraft).not_to be_valid
   end
+
+  describe '#move_forward' do
+    it 'moves the spacecraft one step forward in the current direction' do
+      spacecraft = Spacecraft.new(x: 0, y: 0, z: 0, direction: 'N')
+      spacecraft.move_forward
+      expect(spacecraft.y).to eq(1)
+    end
+  end
 end
